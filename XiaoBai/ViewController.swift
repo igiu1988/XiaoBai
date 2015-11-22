f5fda2ddd32607fd056de21f2cd5ce9ea7c1e4a0
汪洋 <igiuify@gmail.com>
2015-11-22 22:30:45 +0800

    添加 Podfile-SnapKit

--- XiaoBai/ViewController.swift
+++ XiaoBai/ViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let label = UILabel.init()
+        label.backgroundColor = UIColor.yellowColor()
+        self.view.addSubview(label)
+        label.snp_makeConstraints { (make) -> Void in
+            make.top.equalTo(self.view).offset(15)
+            make.left.equalTo(self.view).offset(15)
+        }
+        
+        
+        let note = XBNote.init()
+        note.date = NSDate.init()
+        print(note.date)
+        label.text = note.date.description
     }
 
     override func didReceiveMemoryWarning() {
---
+++ XiaoBai/XBNote.swift
@@ -0,0 +1,18 @@
+//
+//  XBNote.swift
+//  XiaoBai
+//
+//  Created by wangyang on 15/11/22.
+//  Copyright © 2015年 com.wy. All rights reserved.
+//
+
+import UIKit
+
+class XBNote {
+    
+    var date : NSDate = NSDate.init()
+    
+//    init() {
+//        self.date = NSDate.init()
+//    }
+}
