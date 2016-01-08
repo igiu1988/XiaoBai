4a613549ff9e388a397041629c5d428882f244ac
汪洋 <igiuify@gmail.com>
2016-01-08 08:29:09 +0800

    save

--- XiaoBai/ViewController.swift
+++ XiaoBai/ViewController.swift
@@ -8,12 +8,15 @@
 
 import UIKit
 import SnapKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var segment: UISegmentedControl!
+    var duration: Float = 0.0
     
-    
+    let motionManager = CMMotionManager()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         edgesForExtendedLayout = .None;
@@ -21,6 +24,22 @@ class ViewController: UIViewController {
         addChildViewControllers([XBBudgetViewController(), XBExpensesViewController(), XBIncomeViewController()])
         segment.selectedSegmentIndex = NSUserDefaults.standardUserDefaults().integerForKey(segmentIndex)
         segmentChange(nil)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "flip", name: UIDeviceOrientationDidChangeNotification, object: nil)
+        UIDevice.currentDevice().beginGeneratingDeviceOrientationNotifications()
+        
+        
+        motionManager.deviceMotionUpdateInterval = NSTimeInterval(1.0/2.0)
+//        motionManager.accelerometerUpdateInterval = NSTimeInterval(1.0/50.0)
+
+        
+    }
+    
+    func flip() {
+        let orientation = UIDevice.currentDevice().orientation
+        if orientation == .FaceDown {
+            print("faceDown")
+        }
     }
 
     @IBAction func segmentChange(sender: UISegmentedControl?) {
@@ -30,6 +49,20 @@ class ViewController: UIViewController {
     @IBAction func moreAction(sender: AnyObject) {
         
     }
+    @IBAction func startAction(sender: AnyObject) {
+        motionManager.startDeviceMotionUpdatesToQueue(NSOperationQueue.mainQueue()) { (deviceMotion : CMDeviceMotion?, error: NSError?) -> Void in
+            let userAcceleration = deviceMotion?.userAcceleration
+            let rotationRate = deviceMotion?.rotationRate
+            
+            //            let totalAcceleration = sqrt(userAcceleration?.x * userAcceleration?.x +
+            //                userAcceleration?.y * userAcceleration?.y + userAcceleration?.z * userAcceleration?.z)
+            
+//            print("x=\(userAcceleration?.x) y=\(userAcceleration?.y) z=\(userAcceleration?.z)")
+            print("x=\(rotationRate?.x) y=\(rotationRate?.y) z=\(rotationRate?.z)")
+        }
+    }
     
-}
-
+    @IBAction func endAction(sender: AnyObject) {
+        motionManager.stopDeviceMotionUpdates()
+    }
+}rotationRate
